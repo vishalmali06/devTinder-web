4f1c4fee4982cc5fe01e5fcf084f635e20c90d6a
Vishal Mali <vishalmali06@gmail.com>
2025-11-16 23:29:23 +0530

    feat: isPremium added

--- src/components/Premium.jsx
+++ src/components/Premium.jsx
@@ -1,8 +1,19 @@
 import axios from 'axios';
 import { BASE_URL } from '../utils/constants';
+import { useState } from 'react';
 
 const Premium = () => {
 
+    const [isUserPremium, setIsuserPremium] = useState(false);
+
+    const verifyPremiumUser = async () => {
+        const res = await axios.get(BASE_URL + "/premium/verify/", {
+            withCredentials: true,
+        })
+        if (res.data.isPremium) {
+            setIsuserPremium(true);
+        }
+    };
 
     const hnadleBuyClick = async (tyep) => {
         const order = await axios.post(BASE_URL + "/payment/create",
@@ -29,38 +40,38 @@ const Premium = () => {
             theme: {
                 color: '#F37254'
             },
+            handler: verifyPremiumUser,
         };
         const rzp = new window.Razorpay(options);
         rzp.open();
     };
 
-    return (
-        <div className='m-10'>
-            <div className="flex w-full">
-                <div className="card bg-base-300 rounded-box grid h-80 grow place-items-center">
-                    <h1 className='font-bold text-3xl'>Silver Membership</h1>
-                    <ul>
-                        <li> - Chat with other people</li>
-                        <li> - 100 connection Request per day</li>
-                        <li> - Blue Tick</li>
-                        <li> - 3 Months</li>
-                    </ul>
-                    <button onClick={() => hnadleBuyClick("silver")} className='btn btn-secondary'>Buy Silver</button>
-                </div>
-                <div className="divider divider-horizontal">OR</div>
-                <div className="card bg-base-300 rounded-box grid h-80 grow place-items-center">
-                    <h1 className='font-bold text-3xl'>Gold Membership</h1>
-                    <ul>
-                        <li> - Chat with other people</li>
-                        <li> - Infinite connection Request per day</li>
-                        <li> - Blue Tick</li>
-                        <li> - 6 Months</li>
-                    </ul>
-                    <button onClick={() => hnadleBuyClick("gold")} className='btn btn-primary'>Buy Silver</button>
-                </div>
+    return isUserPremium ? "You are already a Premium User !!!" : (<div className='m-10'>
+        <div className="flex w-full">
+            <div className="card bg-base-300 rounded-box grid h-80 grow place-items-center">
+                <h1 className='font-bold text-3xl'>Silver Membership</h1>
+                <ul>
+                    <li> - Chat with other people</li>
+                    <li> - 100 connection Request per day</li>
+                    <li> - Blue Tick</li>
+                    <li> - 3 Months</li>
+                </ul>
+                <button onClick={() => hnadleBuyClick("silver")} className='btn btn-secondary'>Buy Silver</button>
+            </div>
+            <div className="divider divider-horizontal">OR</div>
+            <div className="card bg-base-300 rounded-box grid h-80 grow place-items-center">
+                <h1 className='font-bold text-3xl'>Gold Membership</h1>
+                <ul>
+                    <li> - Chat with other people</li>
+                    <li> - Infinite connection Request per day</li>
+                    <li> - Blue Tick</li>
+                    <li> - 6 Months</li>
+                </ul>
+                <button onClick={() => hnadleBuyClick("gold")} className='btn btn-primary'>Buy Silver</button>
             </div>
         </div>
-    )
-}
+    </div>)
+
+};
 
 export default Premium
